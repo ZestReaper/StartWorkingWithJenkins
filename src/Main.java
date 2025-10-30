ca56c03126fd60f05d0cf1c3c87c0a0cca7c36f4
ZestReaper <zestzestzest1986@gmail.com>
2025-10-30 12:09:58 +0000

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI/CD!");
-        System.out.println("Result: " + calculate(6, 3));
+        System.out.println("Result: " + calculate(7, 3));
     }
     
     public static int calculate(int a, int b) {
